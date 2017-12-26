c56082f0a75462e409db2c0e29aee04c29fec50a
Undo1 <parker@erwaysoftware.com>
2017-12-25 22:21:47 -0700

    Remove metasmoke authorizers

--- authorizers/users.py
+++ authorizers/users.py
@@ -80,31 +80,3 @@ def authorize(event, context):
     }
     log.debug(response)
     return response
-
-
-def authorize_metasmoke(event, context):
-    """ Check if user is authorized to use this route and is metasmoke """
-    token = event['authorizationToken']
-    log.debug("Token: {}".format(token))
-    principalId = token
-    context = {
-        'simpleAuth': True,
-    }
-
-    table = dynamodb.Table(os.environ['ACCESSTOKENS_TABLE'])
-    dbresponse = table.scan(
-        FilterExpression=Attr('token').eq(token) & Attr('enabled').eq(True) & Attr('metasmoke').eq(True)
-    )
-    if len(dbresponse['Items']) == 1:
-        policy = generatePolicy('allow', event['methodArn'])
-        context['user'] = dbresponse['Items'][0]['name']
-    else:
-        policy = generatePolicy('deny', event['methodArn'])
-
-    response = {
-        'principalId': principalId,
-        'policyDocument': policy,
-        'context': context
-    }
-    log.debug(response)
-    return response
